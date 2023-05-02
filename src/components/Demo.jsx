1b5000cc52b1f0000461697eb8611b3b0733b79b
maheshhbalwan <85355816+maheshhbalwan@users.noreply.github.com>
2023-05-02 06:39:11 +0530

    Hero ui and Demo ui

---
+++ src/components/Demo.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const handleSubmit = async (e) => {
+    alert("Submitted");
+  };
+  return (
+    <section className="w-full max-w-xl mt-16">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex items-center justify-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link-icon"
+            className="absolute left-0 w-5 my-2 ml-3"
+          />
+
+          <input
+            type="url"
+            placeholder="Paste the article link"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
+          >
+            <p>↵</p>
+          </button>
+        </form>
+        {/* Browse History */}
+      </div>
+
+      {/* Display Results */}
+    </section>
+  );
+};
+
+export default Demo;
